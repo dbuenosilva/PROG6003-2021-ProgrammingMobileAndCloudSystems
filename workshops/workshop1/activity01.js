b929fde8d7597445643f940c9a8797d9453ae8de
Diego Bueno <diego@gwaya.com>
2021-07-19 14:36:18 +1000

    workshop 2

---
+++ workshops/workshop1/activity01.js
@@ -0,0 +1,4 @@
+var units = new Array();
+units.push("programming");
+units.push("programming");
+console.log(units.length);
---
+++ workshops/workshop1/activity01.ts
@@ -0,0 +1,6 @@
+let units: Array<string> = new Array<string>();
+
+units.push("programming");
+units.push("programming");
+
+console.log(units.length);
